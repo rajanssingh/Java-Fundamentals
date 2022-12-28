e788882e0f901c3695b518244ea8f3542855e217
Rajan Singh <rjnkumar628@gmail.com>
2022-12-28 19:56:24 +0530

    GroupBy

--- JavaStreams/src/test/java/com/rajancodes/examples/GroupingData.java
+++ JavaStreams/src/test/java/com/rajancodes/examples/GroupingData.java
@@ -1,17 +1,27 @@
 package com.rajancodes.examples;
 
 
+import com.google.gson.internal.bind.util.ISO8601Utils;
 import com.rajancodes.beans.Car;
 import com.rajancodes.mockdata.MockData;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class GroupingData {
 
     @Test
     public void simpleGrouping() throws Exception {
-        List<Car> cars = MockData.getCars();
+        List<Car> carList = MockData.getCars();
+        Map<String,List<Car>> groupedData = carList.stream().collect(Collectors.groupingBy(Car::getMake));
+        groupedData.forEach((make,cars) -> {
+            System.out.println(make);
+            cars.forEach(System.out::println);
+        });
     }
 
     @Test
@@ -27,6 +37,10 @@ public class GroupingData {
                 "Alex",
                 "Alex"
         );
+        names.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .forEach((name,count) -> {
+                    System.out.println(name + " > " + count);
+                });
     }
 
 }
