3c04c595ccdfdc36511fee6b0976392e256dd839
Rajan Singh <rjnkumar628@gmail.com>
2025-11-10 21:37:40 +0530

    Java Streams Practice questions

--- src/main/java/EmployeeQueriesStreams/Queries.java
+++ src/main/java/EmployeeQueriesStreams/Queries.java
@@ -6,20 +6,20 @@ import java.util.stream.Collectors;
 public class Queries {
     public static void main(String[] args) {
         List<Employee> employeeList = new ArrayList<>();
-        employeeList.add(new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0));
+        employeeList.add(new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 250000.0));
         employeeList.add(new Employee(122, "Paul Niksui", 25, "Male", "Sales And Marketing", 2015, 13500.0));
         employeeList.add(new Employee(133, "Martin Theron", 29, "Male", "Infrastructure", 2012, 18000.0));
         employeeList.add(new Employee(144, "Murali Gowda", 28, "Male", "Product Development", 2014, 32500.0));
         employeeList.add(new Employee(155, "Nima Roy", 27, "Female", "HR", 2013, 22700.0));
         employeeList.add(new Employee(166, "Iqbal Hussain", 43, "Male", "Security And Transport", 2016, 10500.0));
-        employeeList.add(new Employee(177, "Manu Sharma", 35, "Male", "Account And Finance", 2010, 27000.0));
+        employeeList.add(new Employee(177, "Manu Sharma", 35, "Male", "Account And Finance", 2010, 59000.0));
         employeeList.add(new Employee(188, "Wang Liu", 31, "Male", "Product Development", 2015, 34500.0));
         employeeList.add(new Employee(199, "Amelia Zoe", 24, "Female", "Sales And Marketing", 2016, 11500.0));
         employeeList.add(new Employee(200, "Jaden Dough", 38, "Male", "Security And Transport", 2015, 11000.5));
         employeeList.add(new Employee(211, "Jasna Kaur", 27, "Female", "Infrastructure", 2014, 15700.0));
         employeeList.add(new Employee(222, "Nitin Joshi", 25, "Male", "Product Development", 2016, 28200.0));
         employeeList.add(new Employee(233, "Jyothi Reddy", 27, "Female", "Account And Finance", 2013, 21300.0));
-        employeeList.add(new Employee(244, "Nicolus Den", 24, "Male", "Sales And Marketing", 2017, 10700.5));
+        employeeList.add(new Employee(244, "Nicolus Den", 24, "Male", "Sales And Marketing", 2017, 100700.5));
         employeeList.add(new Employee(255, "Ali Baig", 23, "Male", "Infrastructure", 2018, 12700.0));
         employeeList.add(new Employee(266, "Sanvi Pandey", 26, "Female", "Product Development", 2015, 28900.0));
         employeeList.add(new Employee(277, "Anuj Chettiar", 31, "Male", "Product Development", 2012, 35700.0));
@@ -116,6 +116,18 @@ public class Queries {
                 .max(Comparator.comparing(Employee::getAge))
                 .orElse(null);
         System.out.println(oldestEmployee);
+
+        // 16. Highest employee in every bracket ( < 60k and > 60k)
+        Map<Boolean, Optional<Employee>> collect = employeeList.stream().collect(Collectors.groupingBy(e -> e.salary < 60000, Collectors.maxBy((e1, e2) -> (int) (e1.getSalary() - e2.getSalary()))));
+        System.out.println("16-----------------------");
+        collect.entrySet().stream().forEach(System.out::println);
+
+        // 16. total salary expenditure in every bracket ( < 60k and > 60k)
+        Map<Boolean, Double> collect1 = employeeList.stream().collect(Collectors.groupingBy(e -> e.salary < 60000, Collectors.summingDouble(Employee::getSalary)));
+        Map<Boolean, Double> collect2 = employeeList.stream().collect(Collectors.groupingBy(e -> e.salary < 60000, Collectors.reducing(0.0, Employee::getSalary, Double::sum)));
+        System.out.println("16-----------------------");
+        collect1.entrySet().stream().forEach(System.out::println);
+        collect2.entrySet().stream().forEach(System.out::println);
     }
 
     private static <T> void print(Collection<T> collection){
